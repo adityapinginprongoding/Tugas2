4e167eb67a5b2bd32d24e39a2696a612803553d8
adityapinginprongoding <151723859+adityapinginprongoding@users.noreply.github.com>
2024-12-02 17:37:37 +0700

    kiw kiw

---
+++ matematika.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mtkk;
+
+/**
+ *
+ * @author irvan
+ */
+public class matematika {
+    
+    //pertambahan
+    void pertambahan (double a, double b, double c){
+        double hasil = a + b + c;
+        System.out.println("HASIL PERTAMBAHAN: "+ hasil);
+    };
+    
+    void pertambahan(int a, int b){
+        double hasil = a + b ;
+        System.out.println("HASIL PERTAMBAHAN: "+ hasil);
+    };
+    
+    void pertambahan(int a, int b, int c ){
+        double hasil = a + b + c ;
+        System.out.println("HASIL PERTAMBAHAN: " + hasil);
+    };
+    
+    void pertambahan(double a, double b ){
+        double hasil = a + b  ;
+        System.out.println("HASIL PERTAMBAHAN:" + hasil);
+    };
+    
+    
+    //pengurangan
+    void pengurangan (double a, double b, double c){
+        double hasil = a - b - c;
+        System.out.println("HASIL PENGURANGAN: "+ hasil);
+    };
+    
+    void pengurangan(int a, int b){
+        double hasil = a - b ;
+        System.out.println("HASIL PENGURANGAN:" + hasil);
+    };
+    
+    void pengurangan(int a, int b, int c ){
+        double hasil = a - b - c ;
+        System.out.println("HASIL PENGURANGAN:" + hasil);
+    };
+    
+    void pengurangan(double a, double b ){
+        double hasil = a - b  ;
+        System.out.println("HASIL PENGURANGAN:" + hasil);
+    };
+    
+    
+    //perkalian
+    void perkalian (double a, double b, double c){
+        double hasil = a * b * c;
+        System.out.println("HASIL PERKALIAN:" + hasil);
+    };
+    
+    void perkalian(int a, int b){
+        double hasil = a * b ;
+        System.out.println("HASIL PERKALIAN:" + hasil);
+    };
+    
+    void perkalian(int a, int b, int c ){
+        double hasil = a * b * c ;
+        System.out.println("HASIL PERKALIAN:" + hasil);
+    };
+    
+    void perkalian(double a, double b ){
+        double hasil = a * b  ;
+        System.out.println("HASIL PERKALIAN:" + hasil);
+    };
+    
+    
+    //PEMBAGIAN
+    void pembagian (double a, double b, double c){
+        double hasil = a / b / c;
+        System.out.println("HASIL PEMBAGIAN:" + hasil);
+    };
+    
+    void pembagian(int a, int b){
+        double hasil = a / b ;
+        System.out.println("HASIL PEMBAGIAN:" + hasil);
+    };
+    
+    void pembagian(int a, int b, int c ){
+        double hasil = a / b / c ;
+        System.out.println("HASIL PEMBAGIAN:" + hasil);
+    };
+    
+    void pembagian(double a, double b ){
+        double hasil = a / b  ;
+        System.out.println("HASIL PEMBAGIAN:" + hasil);
+    };
+    
+    
+}
---
+++ matematikaCanggih.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mtkk;
+
+/**
+ *
+ * @author irvan
+ */
+public class matematikaCanggih extends matematika  {
+    
+void modulus (double a, double b, double c){
+        double hasil = a % b % c;
+        System.out.println("HASIL MODULUS: "+ hasil);
+    };
+    
+    void modulus(int a, int b){
+        double hasil = a % b ;
+        System.out.println("HASIL MODULUS: "+ hasil);
+    };
+    
+    void modulus(int a, int b, int c ){
+        double hasil = a % b % c ;
+        System.out.println("HASIL MODULUS: "+ hasil);
+    };
+    
+    void modulus(double a, double b ){
+        double hasil = a % b  ;
+        System.out.println("HASIL MODULUS: "+ hasil);
+    };
+    
+}
